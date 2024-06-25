0f27ffaca612d627d4fbad6c295e06ae193759fb
shaulazain <shaulazain@gmail.com>
2024-06-25 15:29:42 -0700

    Fixed navbar issues and adjusted sidebar styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import BrowseCategories from "../components/browseCategory/BrowseCategories";
 import NftHighlight from "../components/nftHighlight/NftHighlight";
 import DiscoverMoreNfts from "../components/discoverMoreNfts/DiscoverMoreNfts";
 import Hero from "../components/Hero/Hero";
 import TrendingCollection from "../components/trendingCollection/TrendingCollection";
 import HowItWorks from "../components/HowItWorks/HowItWorks";
 import SubscribeWidget from "../components/subscribeWidget/SubscribeWidget";
+import BrowseCategories from "../components/browseCategories/BrowseCategories";
 
 export default function Home(): JSX.Element {
   return (
