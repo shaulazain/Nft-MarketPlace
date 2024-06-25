0a3e34f702933eb81649d3df05e43b1b38167251
shaulazain <shaulazain@gmail.com>
2024-06-24 18:15:56 -0700

    first commit

---
+++ src/components/data/artistName.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { getArtistName } from "../../data/data";
+
+interface ArtistNameProps {
+  creator: string;
+}
+
+export const ArtistName: React.FC<ArtistNameProps> = ({ creator }) => {
+  const [artistName, setArtistName] = useState<string>("");
+
+  useEffect(() => {
+    const fetchArtistName = async () => {
+      if (creator) {
+        const name = await getArtistName(creator);
+        setArtistName(name);
+      }
+    };
+    fetchArtistName();
+  }, [creator]);
+
+  return <span className="text-[16px] lg:text-[22px]">{artistName}</span>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Layout from "./Layout";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./store/Store";
+import { Provider } from "react-redux";
+import Home from "./pages/Home";
+import Category from "./pages/Collections";
+import NFTPage from "./pages/NftPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:collectionSlug",
+        element: <Category />,
+      },
+      {
+        path: "/nft/:contract/:identifier",
+        element: <NFTPage />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
